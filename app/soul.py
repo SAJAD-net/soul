84ff577e8fb8dcc9b286f3f03d126a3eeae2b217
SAJAD <sajadsajad.sch@gmail.com>
2021-08-20 11:14:29 +0430

    updated

--- app/soul.py
+++ app/soul.py
@@ -7,8 +7,8 @@ import time
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-u","--url",required=False,help="url of target")
-#ap.add_argument("-i", "--info",nargs="?",required=True,help="information gathering")
 args=vars(ap.parse_args())
+
 def header_gathering(url):
     agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36"+\
     " (KHTML, like Gecko) Chrome/88.0.4324.182"
@@ -23,14 +23,14 @@ def header_gathering(url):
         result=requests.get("%s"% url,headers=h)
         etime=time.time() - stime
         if result.status_code != 200:
-            print("[!]- eerror --> check your url !")
-            print("total time --> %1.2f"%etime)
+            print("[!]- eerror ~ check your url !")
+            print("total time ~ %1.2f"%etime)
             exit()
         print("[+]- ok, Query 1 Successfully !")
         print("information :\n")
         for k,v in result.headers.items():
-            print("\t %s --> %s"%(k,v))
-        print("total time --> %1.2f"%etime)
+            print("\t %s ~ %s"%(k,v))
+        print("total time ~ %1.2f"%etime)
     except:
         print("[!]- connection error !")
         
@@ -43,8 +43,8 @@ def APP():
             ╚════██║██║   ██║██║   ██║██║     
             ███████║╚██████╔╝╚██████╔╝███████╗
             ╚══════╝ ╚═════╝  ╚═════╝ ╚══════╝""")
-    print("\n\t * WELCOME TO SOUL APP --> H4CK3R V1.0 *\n")
-    url=input("soul >>> ")
+    print("\n\t * WELCOME TO SOUL APP --> H4CK3R V0.5 *\n")
+    url=input("soul ~ ")
     if url == "quit":
         exit()
     header_gathering(url)
